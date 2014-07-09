549195b63bba05e1d884274a7b3ee34424db63da
aa4863586 <xie_w@isumeng.com>
2014-07-09 19:09:25 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SogaApp::Application.config.secret_key_base = '986add89c2dec1bdd0f0ce9512555ab894292f7d7fe574ae0f9b06b8014c2f9dd6c7b37eb13275df83b1ca70fccc01772f4075cfffcc07297f7ec9a06b0eb004'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SogaApp::Application.config.session_store :cookie_store, key: '_soga_app_session'
